734069791e19882b5e1ba5482f4d568005780cb5
Harsh Sharma <hd2002sharma@gmail.com>
2023-03-06 20:21:14 +0530

    files added
    files added

---
+++ src/constants.js
@@ -0,0 +1,6 @@
+const constants = {
+	BASE_URL:"https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=6f102c62f41998d151e5a1b48713cf13&format=json&nojsoncallback=1&extras=url_s&text=cat",
+	STORAGE_KEY: "flicker_search_queries"
+};
+
+export default constants;
